739b40c176e24c189e82c05f44de02cd66dbc447
Jiwon Kim <kim1685@purdue.edu>
2023-12-26 17:18:03 +0000

    fix bug in 2d8e03a: execute primitive w/o args

--- src/bm_sim/actions.cpp
+++ src/bm_sim/actions.cpp
@@ -409,9 +409,9 @@ ActionFnEntry::execute(Packet *pkt) const {
         : primitive.get_source_info()->get_source_fragment());
     param_offset = primitive.get_param_offset();
     // FIXME(jiwon): action_fn (string parsing error) can have no params
-    if (action_fn->params.size() > param_offset) {
-        primitive.execute(&state, &(action_fn->params[param_offset]));
-    }
+    primitive.execute(&state, (action_fn->params.size() > param_offset) ?
+            &(action_fn->params[param_offset]) : nullptr);
+
     idx = primitive.get_jump_offset(idx);
   }
 }
